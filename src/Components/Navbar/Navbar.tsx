3a4ac1a6d6ad435b241126b9b4ffa3814e1acc48
moshaosama <mohamedOSFekry@gmail.com>
2025-03-02 23:57:58 +0200

    Set Navbar Fixed

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -5,30 +5,43 @@ import { NavLink } from "react-router";
 
 const Navbar = () => {
   const { NavbarLink, Icons, PhoneNumber } = Style;
+
+  window.addEventListener("scroll", () => {
+    const Navbar = document.getElementById("Navbar");
+    if (Navbar) {
+      if (window.scrollY >= 10) {
+        Navbar.style.setProperty("background-color", "red");
+      } else {
+        Navbar.style.setProperty("background-color", "transparent");
+      }
+    }
+  });
   return (
-    <div className={Container}>
-      <img
-        src="src\assets\logo-2n.png"
-        alt="Logo.png"
-        loading="lazy"
-        className="w-54"
-      />
+    <div id="Navbar">
+      <div className={`${Container}`}>
+        <img
+          src="src\assets\logo-2n.png"
+          alt="Logo.png"
+          loading="lazy"
+          className="w-54"
+        />
 
-      <div className={NavbarLink}>
-        <NavLink to={"/"}>HOME</NavLink>
-        <NavLink to={"/about"}>ABOUT</NavLink>
-        <NavLink to={"/services"}>SERVICES</NavLink>
-        <NavLink to={"/blogs"}>BLOG</NavLink>
-        <NavLink to={"/shop"}>SHOP</NavLink>
-        <NavLink to={"/faq"}>FAQ</NavLink>
-        <NavLink to={"/contact"}>CONTACT</NavLink>
-      </div>
+        <div className={NavbarLink}>
+          <NavLink to={"/"}>HOME</NavLink>
+          <NavLink to={"/about"}>ABOUT</NavLink>
+          <NavLink to={"/services"}>SERVICES</NavLink>
+          <NavLink to={"/blogs"}>BLOG</NavLink>
+          <NavLink to={"/shop"}>SHOP</NavLink>
+          <NavLink to={"/faq"}>FAQ</NavLink>
+          <NavLink to={"/contact"}>CONTACT</NavLink>
+        </div>
 
-      <div className={Icons}>
-        <FaFacebook className="cursor-pointer" />
-        <FaInstagram className="cursor-pointer" />
-        <FaTwitter className="cursor-pointer" />
-        <div className={PhoneNumber}>+20 1004365707</div>
+        <div className={Icons}>
+          <FaFacebook className="cursor-pointer" />
+          <FaInstagram className="cursor-pointer" />
+          <FaTwitter className="cursor-pointer" />
+          <div className={PhoneNumber}>+20 1004365707</div>
+        </div>
       </div>
     </div>
   );

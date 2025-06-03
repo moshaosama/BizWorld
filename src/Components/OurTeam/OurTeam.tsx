11da91eaeadb4ce5889cb157b9ccb640ffa7bed0
moshaosama <mohamedOSFekry@gmail.com>
2025-06-04 02:12:27 +0300

    Update OurTeam component

--- src/Components/OurTeam/OurTeam.tsx
+++ src/Components/OurTeam/OurTeam.tsx
@@ -13,13 +13,16 @@ const OurTeam = () => {
     return FiguresList?.map((el: FiguresType, index: number) => {
       return (
         <div
-          className="w-1/2 relative transition-all opacity-30 hover:opacity-100 duration-500 hover:-translate-y-10"
+          className="w-1/2 group relative transition-all opacity-30 hover:opacity-100 duration-500 hover:-translate-y-10"
           key={index}
         >
           <img src={el.Image} alt="Rest.png" loading="lazy" decoding="async" />
           <h1 className="absolute bottom-20 left-10 text-3xl text-white z-50">
             {el.Title}
           </h1>
+          <button className="absolute hidden group-hover:block cursor-pointer bottom-5 left-10 text-xl bg-white w-40 py-2 rounded-full text-black font-bold z-50">
+            Show More
+          </button>
         </div>
       );
     });
